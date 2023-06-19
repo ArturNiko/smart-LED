035f60568900c2331d83534dfd8fa918c3617e5f
Artur Papikian <art.niko.super@gmail.com>
2023-06-19 22:10:29 +0200

    working ultrasonic sensor code

--- src/main.rs
+++ src/main.rs
@@ -3,31 +3,77 @@
 
 use esp_backtrace as _;
 use esp_println::println;
-use hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, timer::TimerGroup, Rtc};
+
+use hal::{
+    clock::ClockControl, gpio::IO, peripherals::Peripherals, prelude::*, timer::TimerGroup, Delay,
+    Rtc,
+};
+
 #[entry]
 fn main() -> ! {
+    //const SOUND_SPEED: f32 = 0.034;
+
     let peripherals = Peripherals::take();
     let mut system = peripherals.DPORT.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
-    // Disable the RTC and TIMG watchdog timers
-    let mut rtc = Rtc::new(peripherals.RTC_CNTL);
-    let timer_group0 = TimerGroup::new(
+    let mut timer_group0 = TimerGroup::new(
         peripherals.TIMG0,
         &clocks,
         &mut system.peripheral_clock_control,
     );
-    let mut wdt0 = timer_group0.wdt;
-    let timer_group1 = TimerGroup::new(
-        peripherals.TIMG1,
-        &clocks,
-        &mut system.peripheral_clock_control,
-    );
-    let mut wdt1 = timer_group1.wdt;
-    rtc.rwdt.disable();
-    wdt0.disable();
-    wdt1.disable();
-    println!("Hello world!");
+    let mut wdt = timer_group0.wdt;
+    let mut rtc = Rtc::new(peripherals.RTC_CNTL);
 
-    loop {}
+    // Disable MWDT and RWDT (Watchdog) flash boot protection
+    wdt.disable();
+    rtc.rwdt.disable();
+
+    // Set GPIO5 as an output and GPIO15 analog input.
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+
+    let mut trig = io.pins.gpio2.into_push_pull_output();
+    let echo = io.pins.gpio15.into_floating_input();
+
+    
+
+    // Initialize the Delay peripheral and activate timer.
+    let mut delay: Delay = Delay::new(&clocks);
+    timer_group0.timer0.set_counter_active(true);
+
+
+    loop {
+        // Clears the trigPin
+        trig.set_low().unwrap();
+        delay.delay_ms(5_u32);
+        
+        // Sets the trigPin on HIGH state for 10 micro seconds
+        trig.set_high().unwrap();
+        delay.delay_ms(10_u32);
+        trig.set_low().unwrap();
+
+
+        // Wait until pin goes high
+        while !echo.is_high().unwrap() {}
+
+        // Kick off timer measurement
+        let echo_start = timer_group0.timer0.now();
+
+        // Wait until pin goes low
+        while !echo.is_low().unwrap() {}
+
+        // Collect current timer count
+        let echo_end = timer_group0.timer0.now();
+
+    
+        // Calculate the elapsed timer count
+        let echo_dur = echo_end - echo_start;
+
+        // Calculate the distance in cms using formula in datasheet
+        let distance_cm = echo_dur / 16 / 58;
+
+        // Print the distance output
+        println!("Distance {} cm\r", distance_cm);
+    }
+    
 }
